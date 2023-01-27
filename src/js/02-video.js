154a43e9035bf50c58a005a9b5f7fd03008045a4
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-01-27 12:40:39 +0200

    Выполнил задачу 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,41 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-localStorage.setItem('videoplayer-current-time', 'onPlay');
-const currentVideoTime = localStorage.getItem('videoplayer-current-time');
-console.log(`Eto local storage`, currentVideoTime);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+let currentVideoTime = localStorage.getItem('videoplayer-current-time') || 0;
 
 const onPlay = function (data) {
-  console.log(`🚀 ~ onPlay ~ data`, data.seconds);
-  let currentTime = data.seconds;
-  return currentTime;
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player
-  .setCurrentTime(currentVideoTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(currentVideoTime);
